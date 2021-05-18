32ce6f0cbfb22ee5fe4888b984b1ea6382bef594
Laura Gashi <laura.gashi1@student.uni-pr.edu>
2021-05-18 13:57:14 +0200

    Change

--- README.md
+++
@@ -1,5 +0,0 @@
-# INT20_21_Gr15
-Projekti semestral nga lenda Interneti
-
-
-[About Corona](https://int20-21-corona.netlify.app/)
--- lo.js
+++
@@ -1,5 +0,0 @@
-let array=[5,1,8];
-array.forEach((Element)=>{
-    console.log(Element)});
-    array.reverse();
-    console.log(array);
