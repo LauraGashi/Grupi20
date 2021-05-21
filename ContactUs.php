39b27a11113b06cca70ede420c6f2fd01fd941cf
LiridonaBaliu <liridona.baliu@student.uni-pr.edu>
2021-05-21 22:03:18 +0100

    Update ContactUs.php

--- ContactUs.php
+++ ContactUs.php
@@ -109,6 +109,25 @@ function test_input($data) {
 }
 ?>
 
+<?php
+// Starting the session
+session_start();
+
+// Storing the value in session
+$_SESSION['id'] = 342;
+
+// conditional usage of session values that may have been set in a previous session
+if(!isset($_SESSION["login"])) {
+    echo "Please login first";
+    exit;
+}
+// now you can use the login safely
+$user = $_SESSION["login"];
+?>
+
+
+
+
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
