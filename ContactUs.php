28b1bb70f3ec36640e634b082bb58ae741583bf4
Laura Gashi <laura.gashi1@student.uni-pr.edu>
2021-05-21 15:58:18 +0200

    Update ContactUs.php

--- ContactUs.php
+++ ContactUs.php
@@ -132,18 +132,7 @@ function test_input($data) {
   <input type="submit" name="submit" value="Submit">  
 </form>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
+
         </div>
 
         <div class="text" style="margin-left: 30px;">
